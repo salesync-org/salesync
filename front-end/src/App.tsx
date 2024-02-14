eaed16e0f7e23c53c40d509a3c97116fa668c114
Duy Quang <vanduyquang2020@gmail.com>
2024-02-15 02:52:42 +0700

    Set up tailwind and UI cheatsheet

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      
+      <h1 className="mx-20 text-3xl font-bold hover:text-pink-900">
+      Hello world!
+    </h1>
     </>
   )
 }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -5,8 +5,43 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      lineHeight: {
+        '1.5': '1.5',
+      },
+      fontWeight: {
+        '400': '400',
+      },
+      colors: {
+        background: {
+          'light': '#f3f4f6',
+          'DEFAULT': '#1a202c',
+          'dark': '#000000',
+        },
+        panel: {
+          'light': '#ffffff',
+          'DEFAULT': '#2d3748',
+          'dark': '#000000',
+        },
+        primary: {
+          'light': '#f6ad55',
+          'DEFAULT': '#f6ad55',
+          'dark': '#f6ad55',
+          'alight': '#f6ad55',
+          'adark': '#f6ad55',
+          'hlight': '#f6ad55',
+          'hdark': '#f6ad55',
+          'plight': '#f6ad55',
+          'pdark': '#f6ad55',
+          'dlight': '#f6ad55',
+          'ddark': '#f6ad55',
+        },
+      }
+    },
   },
   plugins: [],
-}
+}; // Add a semicolon here
 

847b4ec3ddfa070fedd098700bf1fbdeb7532200
20120188 <20120188@student.hcmus.edu.vn>
2024-02-21 11:26:49 +0700

    Add back-end folder and init Spring boot project

---
+++ back-end/type-service/src/main/java/com/salesync/typeservice/TypeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.salesync.typeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeServiceApplication.class, args);
+	}
+
+}

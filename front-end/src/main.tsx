16412fad8852bcdd827bf7aac0e6e2fef4585879
mtoan12 <tranminhtoan1280@gmail.com>
2024-02-21 14:25:52 +0700

    Set up for React Query

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('entry')!).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
     </React.StrictMode>
 );

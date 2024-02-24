e9f987ac878772abda3ce644255a2cb57de9f91b
Duy Quang <vanduyquang2020@gmail.com>
2024-02-24 10:44:57 +0700

    Separate logic in DropDown to custom hooks (useDropDown and useClickOutside)

--- front-end/src/components/DropDown/DropDown.tsx
+++ front-end/src/components/DropDown/DropDown.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Button from '../Button/Button';
-import Icon from '../Icon/Icon';
+import React from "react";
+import Button from "../Button/Button";
+import Icon from "../Icon/Icon";
+import { useDropDown } from "../../hooks/useDropDown";
+import { cn } from "../../utils/utils";
 
 interface DropdownButtonProps {
     value: string;
@@ -17,47 +19,16 @@ const DropDown: React.FC<DropdownButtonProps> = ({
     disabled,
     showHeader = true,
 }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [shoulDropUp, setDropDirection] = useState(false);
-    const [selectedOption, setSelectedOption] = useState('');
-    const dropdownRef = useRef<HTMLDivElement>(null);
-    const buttonRef = useRef<HTMLDivElement>(null);
-    const menuRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        if (!isOpen) {
-            return;
-        }
-        const dropdownElement = dropdownRef.current;
-
-        if (dropdownElement) {
-            const windowHeight = window.innerHeight;
-            const dropdownRect = dropdownElement.getBoundingClientRect();
-            const spaceBelow = windowHeight - dropdownRect.bottom;
-
-            // Adjust the condition based on your specific requirements
-            setDropDirection(
-                spaceBelow > 20 * parseFloat(getComputedStyle(document.documentElement).fontSize)
-            );
-        }
-    }, [isOpen]);
-
-    useEffect(() => {
-        const handleMouseDown = (event: MouseEvent) => {
-            if (
-                dropdownRef.current?.contains(event.target as Node) ||
-                buttonRef.current?.contains(event.target as Node) ||
-                menuRef.current?.contains(event.target as Node)
-            ) {
-                return;
-            }
-            setIsOpen(false);
-        };
-        window.addEventListener('mousedown', handleMouseDown);
-        return () => {
-            window.removeEventListener('mousedown', handleMouseDown);
-        };
-    }, []);
+    const {
+        isOpen,
+        setIsOpen,
+        shoulDropUp,
+        selectedOption,
+        setSelectedOption,
+        buttonContentRef,
+        buttonRef,
+        menuRef,
+    } = useDropDown();
 
     const handleOptionClick = (option: string) => {
         setSelectedOption(option);
@@ -65,13 +36,13 @@ const DropDown: React.FC<DropdownButtonProps> = ({
     };
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'Escape') {
+        if (event.key === "Escape") {
             setIsOpen(false);
         }
     };
 
     const handleTabKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'Enter') {
+        if (event.key === "Enter") {
             let target = event.target as HTMLElement;
             while (target && target !== event.currentTarget) {
                 if (target.parentNode === event.currentTarget) {
@@ -84,7 +55,11 @@ const DropDown: React.FC<DropdownButtonProps> = ({
     };
 
     return (
-        <div ref={buttonRef} className="dropdown relative" onKeyDown={handleKeyDown}>
+        <div
+            ref={buttonRef}
+            className="dropdown relative"
+            onKeyDown={handleKeyDown}
+        >
             <Button
                 header={header}
                 showHeader={showHeader}
@@ -92,20 +67,22 @@ const DropDown: React.FC<DropdownButtonProps> = ({
                 onClick={() => setIsOpen(!isOpen)}
             >
                 <Icon name="expand_more" />
-                <p className="truncate" ref={dropdownRef}>
+                <p className="truncate" ref={buttonContentRef}>
                     {selectedOption || value}
                 </p>
             </Button>
             <div
                 ref={menuRef}
-                className={`divide-button-stroke-light dark:divide-button-stroke-dark divide-y-2 divide
-                rounded bg-button-background-light dark:bg-button-background-dark px-2 border-2 border-button-stroke-light dark:border-button-stroke-dark
-                absolute z-10 max-h-80 overflow-y-auto ${
-                    shoulDropUp ? 'origin-top top-[4.8rem]' : 'origin-bottom bottom-12'
-                }
-                ${
-                    isOpen ? 'scale-100' : 'scale-0 *:hidden'
-                } transition-all duration-100 ease-in-out`}
+                className={cn(
+                    "rounded px-2 border-2 absolute z-10 max-h-80 overflow-y-auto transition-all duration-100 ease-in-out",
+                    "divide-button-stroke-light dark:divide-button-stroke-dark divide-y-2 divide",
+                    "bg-button-background-light dark:bg-button-background-dark",
+                    "border-button-stroke-light dark:border-button-stroke-dark",
+                    shoulDropUp
+                        ? "origin-top top-[4.8rem]"
+                        : "origin-bottom bottom-12",
+                    isOpen ? "scale-100" : "scale-0 *:hidden"
+                )}
                 onClick={(event) => {
                     let target = event.target as HTMLElement;
                     while (target && target !== event.currentTarget) {

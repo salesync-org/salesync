1ba2a4d172f05c5dcbcbe3e984cff4dd86355a76
20120188 <20120188@student.hcmus.edu.vn>
2024-02-24 12:14:13 +0700

    Mock up type manager page to add new type

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Cheatsheet from './pages/Cheatsheet/Cheatsheet';
+import TypeManager from './pages/TypeManager/TypeManager';
 
 function App() {
   return (
-    <div>
-      <Cheatsheet />
-    </div>
+    // <div>
+    //   <Cheatsheet />
+    // </div>
+
+    <Routes>
+      <Route path='/' element={<Cheatsheet />} />
+      <Route path='/type-manager' element={<TypeManager />} />
+    </Routes>
   );
 }
 

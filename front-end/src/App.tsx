6b0270bb6cf273e7a0def69b5bcfe33bebdc5dea
Duy Quang <vanduyquang2020@gmail.com>
2024-02-24 18:02:38 +0700

    Allow navigating to Cheatsheet page after log in option.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,18 @@
+import { Suspense } from 'react';
 import LogIn from './pages/LogIn/LogIn';
+import { Route, Routes } from 'react-router-dom';
+import Cheatsheet from './pages/Cheatsheet/Cheatsheet';
 
 function App() {
   return (
-    <div>
-      <LogIn />
+      <div>
+    <Suspense>
+        <Routes>
+          <Route path='/' element={<LogIn />} />
+          <Route path='/cheatsheet' element={<Cheatsheet />} />
+          {/* <Route path='*' element={<NotFoundPage />} /> */}
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- front-end/src/pages/LogIn/LogIn.tsx
+++ front-end/src/pages/LogIn/LogIn.tsx
@@ -2,8 +2,15 @@ import Button from '@/components/ui/Button/Button';
 import PrimaryButton from '@/components/ui/Button/PrimaryButton';
 import Icon from '@/components/ui/Icon/Icon';
 import { sendRequest } from './LogInNavigate';
+import { useNavigate } from 'react-router-dom';
 
 function LogIn() {
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/cheatsheet`; 
+    navigate(path);
+  };
+
   return (
     <div className='mx-auto px-10 pt-20'>
       <h1 style={{fontSize: "5rem"}} className='my-20'>Log In Stimulation</h1>
@@ -12,11 +19,11 @@ function LogIn() {
         the options below.
       </p>
       <div className='flex items-center space-x-4 w-fit'>
-        <PrimaryButton className='my-4' showHeader={false} onClick={() => {sendRequest('admin', 'admin')}}>
+        <PrimaryButton className='my-4' showHeader={false} onClick={() => {sendRequest('admin', 'admin'); routeChange();}}>
           <Icon name='check_circle'></Icon>
           <div>Log In</div>
         </PrimaryButton>
-        <Button onClick={() => {sendRequest('notadmin', 'admin')}} showHeader={false}>
+        <Button onClick={() => {sendRequest('notadmin', 'admin'); routeChange();}} showHeader={false}>
           <Icon name='cancel'></Icon>
           <div>Continue as Guest</div>
         </Button>

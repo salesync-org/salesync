ec0f72dccbfdfd53b5746d539f5ea63f5b2d5ca1
Duy Quang <vanduyquang2020@gmail.com>
2024-02-24 11:25:32 +0700

    Style ReactRouterDomPanel to give better example of style usage

--- front-end/src/pages/Cheatsheet/ReactRouterDomPanel/ReactRouterDomPanel.tsx
+++ front-end/src/pages/Cheatsheet/ReactRouterDomPanel/ReactRouterDomPanel.tsx
@@ -5,7 +5,7 @@ function ReactRouterDomPanel() {
   return (
     <Panel>
       <h1 className='mb-5'>React Router Dom</h1>
-      <p>React Router enables "client side routing".</p>
+      <h2 className='mb-5'>Allow Client side routing.</h2>
       <p>
         Client side routing allows your app to update the URL from a link click without making another request for
         another document from the server. Instead, your app can immediately render some new UI and make data requests
@@ -18,10 +18,16 @@ function ReactRouterDomPanel() {
         <NavItem name='Opportunity' path='/opportunity' />
         <NavItem name='Hướng dẫn' path='https://viblo.asia/p/react-router-dom-v6-maGK7BQB5j2' target='_blank' />
       </nav>
-      <h1 className='mb-5'>Các khái niệm sẽ sử dụng: (Xem hướng dẫn để biết chức năng và cách sử dụng)</h1>
-      <p>React elements: Routes, Route, Link, NavLink, Navigate</p>
-      <p>Hooks: useNavigate, useParams, useSearchParams</p>
-      <p>React.lazy: Lazy loading của React (tránh tải toàn bộ cái file js trong import trong lần đầu load trang)</p>
+      <div className='mb-5 space-y-2'>
+        <h2>Các khái niệm sẽ sử dụng:</h2>
+        <p>(Xem hướng dẫn để biết chức năng và cách sử dụng)</p>
+      </div>
+      <h3>React elements:</h3>
+      <p> Routes, Route, Link, NavLink, Navigate</p>
+      <h3>React hooks:</h3>
+      <p>useNavigate, useParams, useSearchParams</p>
+      <h3>React.lazy: </h3>
+      <p>Lazy loading của React (tránh tải toàn bộ cái file js trong import trong lần đầu load trang)</p>
     </Panel>
   );
 }

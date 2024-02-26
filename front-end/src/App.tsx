0b3d3d3388e83b8a1591b4300a5bfe971026adf8
20120188 <20120188@student.hcmus.edu.vn>
2024-02-26 15:25:10 +0700

    Rebase branch to add config and msw

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,24 +3,18 @@ import LogIn from './pages/LogIn/LogIn';
 import { Route, Routes } from 'react-router-dom';
 import Cheatsheet from './pages/Cheatsheet/Cheatsheet';
 import TypeManager from './pages/TypeManager/TypeManager';
-import TypeManager from './pages/TypeManager/TypeManager';
 
 function App() {
   return (
-      // <div>
-    <Suspense>
+    <div>
+      <Suspense>
         <Routes>
           <Route path='/' element={<LogIn />} />
-        //   <Route path='/cheatsheet' element={<Cheatsheet />} />
-          {/* <Route path='*' element={<NotFoundPage />} /> */}
+          <Route path='/cheatsheet' element={<Cheatsheet />} />
+          <Route path='/type-manager' element={<TypeManager />} />
         </Routes>
       </Suspense>
-    // </div>
-
-    <Routes>
-      <Route path='/' element={<Cheatsheet />} />
-      <Route path='/type-manager' element={<TypeManager />} />
-    </Routes>
+    </div>
   );
 }
 

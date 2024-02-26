7664cce99fb9cacd56ebfcdd5f7661ce6152b02a
mtoan12 <tranminhtoan1280@gmail.com>
2024-02-26 14:40:10 +0700

    Create Layout for type manager page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,23 @@
-import Cheatsheet from 'pages/Cheatsheet/Cheatsheet';
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Cheatsheet = lazy(() => import('pages/Cheatsheet/Cheatsheet'));
+const ConfigLayout = lazy(() => import('./components/layout/ConfigLayout'));
+const TypeManager = lazy(() => import('./pages/TypeManager/TypeManager'));
 
 function App() {
   return (
     <div>
-      <Cheatsheet />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Cheatsheet />} />
+          <Route path='/config' element={<ConfigLayout />}>
+            <Route path='type' element={<TypeManager />} />
+            <Route path='field' element={<TypeManager />} />
+            <Route path='link' element={<TypeManager />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }

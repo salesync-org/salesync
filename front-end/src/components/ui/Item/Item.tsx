abbc70415ca4688435b6f1090e4198312e1a64a2
Duy Quang <vanduyquang2020@gmail.com>
2024-02-28 16:47:17 +0700

    Fix Option Item in DropDownList having a Text Cursor

--- front-end/src/components/ui/Item/Item.tsx
+++ front-end/src/components/ui/Item/Item.tsx
@@ -18,7 +18,7 @@ const Item: React.FC<ItemProps> = ({ href, className, title, subTitle, additiona
       <a className={cn(`flex rounded-sm py-2`, className)} href={href} tabIndex={0} title={title}>
         <div
           className={cn(
-            'flex flex-grow items-center rounded-sm px-2 py-2 align-middle',
+            'flex flex-grow items-center rounded-sm px-2 py-2 align-middle cursor-pointer',
             'hover:bg-secondary-light dark:hover:bg-secondary-dark',
             'hover:text-link-text-light  dark:hover:text-link-text-dark'
           )}
@@ -36,10 +36,10 @@ const Item: React.FC<ItemProps> = ({ href, className, title, subTitle, additiona
           )}
           <div className='mx-2 min-w-32 flex-grow flex-nowrap text-ellipsis pr-4 align-middle'>
             <div>
-              <h5 className='text-ellipsis text-nowrap'>{additionalInfo}</h5>
-              {(subTitle || additionalInfo) && <p>{title}</p>}
-              {!subTitle && !additionalInfo && <h4>{title}</h4>}
-              <h5>{subTitle}</h5>
+              <h5 className='text-ellipsis text-nowrap select-none'>{additionalInfo}</h5>
+              {(subTitle || additionalInfo) && <p className='select-none'>{title}</p>}
+              {!subTitle && !additionalInfo && <h4 className='select-none'>{title}</h4>}
+              <h5 className='select-none'>{subTitle}</h5>
             </div>
           </div>
           {href && <Icon name='arrow_outward' size='2rem' />}

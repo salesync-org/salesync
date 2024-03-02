1d11358091ead80884cd09630792d592f1029a38
20120188 <20120188@student.hcmus.edu.vn>
2024-03-02 11:52:19 +0700

    Update API endpoints in typeApi.ts

--- front-end/src/api/typeApi.ts
+++ front-end/src/api/typeApi.ts
@@ -2,14 +2,14 @@ import instance from './axiosConfig';
 
 class TypeApi {
   createType = async ({ typeName, template }: { typeName: String; template: String }) => {
-    const response = await instance.post('http://localhost:8080/api/v1/type/create', {
+    const response = await instance.post('http://localhost:8080/apis/v1/type/create', {
       name: typeName
     });
     return response.data;
   };
 
   getAllTypes = async () => {
-    const response = await instance.get('http://localhost:8080/api/v1/type/get-all');
+    const response = await instance.get('http://localhost:8080/apis/v1/type/get-all');
     return response.data;
   };
 }

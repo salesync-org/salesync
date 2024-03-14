b5b596191b10bdf8afc30878fccb733cae206569
mtoan12 <tranminhtoan1280@gmail.com>
2024-03-14 12:57:58 +0700

    Create a draggable aside nav

--- front-end/src/components/RecordLayout/AsideNav.tsx
+++ front-end/src/components/RecordLayout/AsideNav.tsx
@@ -1,8 +1,17 @@
 import { cn } from '@/utils/utils';
 import { NavLink } from 'react-router-dom';
 import Icon from '../ui/Icon/Icon';
+import { useState } from 'react';
 
-const asideItems = [
+type AsideItem = {
+  icon: string;
+  href: string;
+  title: string;
+  active?: boolean;
+  className?: string;
+};
+
+const asideItems: AsideItem[] = [
   { icon: 'home', href: '/home', title: 'Home' },
   { icon: 'monitoring', href: '/sales', title: 'Sales', active: true },
   { icon: 'ecg_heart', href: '/services', title: 'Services' },
@@ -12,13 +21,22 @@ const asideItems = [
 ];
 
 const AsideNav = () => {
+  const [currentAsideItems, setCurrentAsideItems] = useState<AsideItem[]>(() => {
+    return JSON.parse(localStorage.getItem('asideItems') || '') || asideItems;
+  });
+
   return (
     <aside className='fixed grid min-h-dvh w-[76px] justify-center bg-[#014486] text-white'>
       <nav className='p-1'>
         <ul className='flex flex-col gap-1'>
-          {asideItems.map((item) => (
-            <li key={item.title}>
-              <AsideItem {...item} />
+          {currentAsideItems.map((item, index) => (
+            <li key={item.title} value={index}>
+              <AsideItem
+                {...item}
+                currentAsideItems={currentAsideItems}
+                setCurrentAsideItems={setCurrentAsideItems}
+                selfIndex={index}
+              />
             </li>
           ))}
         </ul>
@@ -31,18 +49,64 @@ const AsideItem = ({
   icon,
   href,
   title,
-  className
+  className,
+  currentAsideItems,
+  setCurrentAsideItems,
+  selfIndex
 }: {
   icon: string;
   href: string;
   title: string;
   active?: boolean;
   className?: string;
+  currentAsideItems: AsideItem[];
+  setCurrentAsideItems: React.Dispatch<React.SetStateAction<AsideItem[]>>;
+  selfIndex: number;
 }) => {
+  const handleDragStart = (e: React.DragEvent<HTMLAnchorElement>) => {
+    e.currentTarget.classList.add('dragging');
+    e.currentTarget.classList.add('rotate-12');
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const target = document.querySelector('.dragging') as HTMLAnchorElement;
+    const siblings = target.parentElement?.parentElement?.children;
+
+    if (!siblings) return;
+
+    const sibling = [...siblings].find((sibling) => {
+      return e.clientY < sibling.getBoundingClientRect().bottom;
+    });
+
+    if (sibling) {
+      const from = selfIndex;
+      const to = Number(sibling.getAttribute('value'));
+      const newTabs = [...currentAsideItems];
+      newTabs.splice(to, 0, newTabs.splice(from, 1)[0]);
+      setCurrentAsideItems(newTabs);
+    }
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDragEnd = (e: React.DragEvent<HTMLAnchorElement>) => {
+    e.currentTarget.classList.remove('rotate-12');
+    e.currentTarget.classList.remove('dragging');
+
+    localStorage.setItem('asideItems', JSON.stringify(currentAsideItems));
+  };
   return (
     <NavLink
       to={href}
       title={title}
+      draggable
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnd={handleDragEnd}
+      onDrop={handleDrop}
       className={({ isActive }) =>
         cn(
           'flex h-16 w-16 flex-col items-center justify-center rounded-[8px]  text-center text-sm leading-5',

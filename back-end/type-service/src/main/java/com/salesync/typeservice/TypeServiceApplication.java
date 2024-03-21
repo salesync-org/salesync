9a904a4b61d8b299c80e2f8fbbcb4b6e47e3ac12
Duy Quang <vanduyquang2020@gmail.com>
2024-03-21 20:41:09 +0700

    Testing GitHub Action Config

--- back-end/type-service/src/main/java/com/salesync/typeservice/TypeServiceApplication.java
+++ back-end/type-service/src/main/java/com/salesync/typeservice/TypeServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class TypeServiceApplication {
-	// Main method
+	// Test GitHub CICD
 	public static void main(String[] args) {
 		SpringApplication.run(TypeServiceApplication.class, args);
 	}

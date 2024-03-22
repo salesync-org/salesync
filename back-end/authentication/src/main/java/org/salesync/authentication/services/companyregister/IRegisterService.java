4cadee40c91cf57bef6018a3420f4b4185dee113
Duy Quang <vanduyquang2020@gmail.com>
2024-03-22 19:08:48 +0700

    Add validation for Keycloak

--- back-end/authentication/src/main/java/org/salesync/authentication/services/companyregister/IRegisterService.java
+++ back-end/authentication/src/main/java/org/salesync/authentication/services/companyregister/IRegisterService.java
@@ -2,6 +2,7 @@ package org.salesync.authentication.services.companyregister;
 
 import jakarta.ws.rs.core.Response;
 import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.JsonWebToken;
 import org.salesync.authentication.dtos.CompanyRegisterDto;
 import org.salesync.authentication.dtos.LogInDto;
 import org.salesync.authentication.dtos.NewUserDto;
@@ -12,4 +13,6 @@ public interface IRegisterService {
     AccessTokenResponse login(String realmName, LogInDto logInDTO, String clientId, String clientSecret);
 
     Response logout(String token);
+
+    AccessTokenResponse validate(String accessToken);
 }

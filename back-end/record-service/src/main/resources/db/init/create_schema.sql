630208b1ec07bb051a9c8607021bd869a98b18e8
LeThaiNhuQuynh <lethainhuquynh@gmail.com>
2024-03-25 20:49:37 +0700

    update record db script

--- back-end/record-service/src/main/resources/db/init/create_schema.sql
+++ back-end/record-service/src/main/resources/db/init/create_schema.sql
@@ -24,7 +24,11 @@ CREATE TABLE IF NOT EXISTS public.record_type_property
     property_id uuid NOT NULL,
     record_property_label text,
     item_value text,
-    CONSTRAINT pk_record_type_property PRIMARY KEY (property_record_id)
+    CONSTRAINT pk_record_type_property PRIMARY KEY (property_record_id),
+    CONSTRAINT fk_record_type_property_record FOREIGN KEY (record_id)
+        REFERENCES public.record (record_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
     );
 
 ALTER TABLE IF EXISTS public.record_type_property
@@ -36,7 +40,15 @@ CREATE TABLE IF NOT EXISTS public.record_type_relation
     source_record_id uuid NOT NULL,
     destination_record_id uuid NOT NULL,
     relation_id uuid NOT NULL,
-    CONSTRAINT pk_record_type_relation PRIMARY KEY (record_relation_id)
+    CONSTRAINT pk_record_type_relation PRIMARY KEY (record_relation_id),
+    CONSTRAINT fk_record_type_relation_source_record FOREIGN KEY (source_record_id)
+        REFERENCES public.record (record_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_record_type_relation_destination_record FOREIGN KEY (destination_record_id)
+        REFERENCES public.record (record_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
     );
 
 ALTER TABLE IF EXISTS public.record_type_relation
@@ -47,7 +59,11 @@ CREATE TABLE IF NOT EXISTS public.record_type
     record_type_id uuid NOT NULL DEFAULT gen_random_uuid(),
     type_id uuid NOT NULL,
     record_id uuid NOT NULL,
-    CONSTRAINT pk_record_type PRIMARY KEY (record_type_id)
+    CONSTRAINT pk_record_type PRIMARY KEY (record_type_id),
+    CONSTRAINT fk_record_type_record FOREIGN KEY (record_id)
+        REFERENCES public.record (record_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
     );
 
 ALTER TABLE IF EXISTS public.record_type
@@ -58,7 +74,11 @@ CREATE TABLE IF NOT EXISTS public.record_stage
     record_stage_id uuid NOT NULL DEFAULT gen_random_uuid(),
     record_id uuid NOT NULL,
     stage_id uuid NOT NULL,
-    CONSTRAINT pk_record_stage PRIMARY KEY (record_stage_id)
+    CONSTRAINT pk_record_stage PRIMARY KEY (record_stage_id),
+    CONSTRAINT fk_record_stage_record FOREIGN KEY (record_id)
+        REFERENCES public.record (record_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
     );
 
 ALTER TABLE IF EXISTS public.record_stage

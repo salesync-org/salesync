08b6053778dfd8f3e9f1e4eea10b0c23d2d46693
mtoan12 <tranminhtoan1280@gmail.com>
2024-04-08 15:59:43 +0700

    Create ActionDropDown

--- front-end/src/components/Relation/RelationSection.tsx
+++ front-end/src/components/Relation/RelationSection.tsx
@@ -1,40 +1,23 @@
-import { useState } from 'react';
-import { Button, DropDownList, Icon, Item, Panel } from '../ui';
+import { Panel } from '../ui';
+import ActionDropDown from '../ui/DropDown/ActionDropDown';
 
 const RelationSection = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+  const createNewRelation = () => {
+    console.log('Create new relation');
+  };
 
+  const actions = [
+    {
+      title: 'New',
+      action: createNewRelation
+    }
+  ];
   return (
     <Panel>
-      <header>
+      <header className='flex items-center justify-between'>
         <h2>Opportunities (2)</h2>
-        <Button
-          rounded='icon'
-          className='mx-0 my-0 h-10 w-10 border-0 px-0 py-0'
-          intent='link'
-          onClick={() => {
-            setMenuOpen(!isMenuOpen);
-          }}
-        >
-          <Icon name='expand_more' />
-        </Button>
-        <DropDownList
-          open={isMenuOpen}
-          onClose={() => {
-            setMenuOpen(false);
-          }}
-          className='mt-0 w-80'
-          divide={false}
-        >
-          <Item
-            className='py-0'
-            title='New'
-            onClick={() => {
-              // navigate('/setting');
-            }}
-          />
-          <div className='h-2' />
-        </DropDownList>
+
+        <ActionDropDown actions={actions} />
       </header>
     </Panel>
   );

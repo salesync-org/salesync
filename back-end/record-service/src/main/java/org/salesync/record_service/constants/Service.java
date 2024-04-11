0984a7009719575189b16ee7fbdc141f5fcf3413
20120188 <20120188@student.hcmus.edu.vn>
2024-04-11 15:03:07 +0700

    fix get all relation api, call type service to get type name and id

---
+++ back-end/record-service/src/main/java/org/salesync/record_service/constants/Service.java
@@ -0,0 +1,7 @@
+package org.salesync.record_service.constants;
+
+public class Service {
+
+    public static final String TYPE_SERVICE = "type-service";
+    public static final String RECORD_SERVICE = "record-service";
+}
---
+++ back-end/record-service/src/main/java/org/salesync/record_service/dtos/TypeDto.java
@@ -0,0 +1,19 @@
+package org.salesync.record_service.dtos;
+
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class TypeDto {
+    UUID id;
+    String name;
+}

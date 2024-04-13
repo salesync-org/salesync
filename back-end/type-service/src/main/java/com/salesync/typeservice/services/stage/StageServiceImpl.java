7eab63e45a77797144c859cd3089582879223d1a
duytruong-23 <zduytruongz@gmail.com>
2024-04-13 10:01:47 +0700

    SSF-58 update a satge

--- back-end/type-service/src/main/java/com/salesync/typeservice/services/stage/StageServiceImpl.java
+++ back-end/type-service/src/main/java/com/salesync/typeservice/services/stage/StageServiceImpl.java
@@ -18,6 +18,7 @@ public class StageServiceImpl implements StageService {
     private final StageRepository stageRepository;
     private final TypeRepository typeRepository;
     private final StageMapper stageMapper = StageMapper.INSTANCE;
+
     @Override
     public StageDto createStage(StageDto stageDto) {
         Type type = typeRepository.findById(stageDto.getType().getId()).orElseThrow(
@@ -36,5 +37,46 @@ public class StageServiceImpl implements StageService {
             return stageMapper.entityToDto(stageRepository.save(stage));
         }
         throw new TypeServiceException("Type is not a stage object");
+    }
+
+    @Override
+    public StageDto updateStage(StageDto stageDto) {
+        Type type = typeRepository.findById(stageDto.getType().getId()).orElseThrow(
+                () -> new ObjectNotFoundException(
+                        Type.class.getSimpleName(),
+                        stageDto.getType().getId().toString()
+                )
+        );
+        if (!TemplateEnum.StageObject.equals(type.getTemplate().getName())) {
+            throw new TypeServiceException("Type is not a stage object");
+        }
+        Stage sourceStage = stageRepository.findById(stageDto.getId()).orElseThrow(
+                () -> new ObjectNotFoundException(
+                        Stage.class.getSimpleName(),
+                        stageDto.getId().toString()
+                )
+        );
+        sourceStage.setName(stageDto.getName() == null ? sourceStage.getName() : stageDto.getName());
+        Integer seq = stageRepository.findTopByTypeIdOrderBySequenceNumberDesc(type.getId())
+                .map(Stage::getSequenceNumber)
+                .orElse(0);
+        if (stageDto.getSequenceNumber() != null &&
+                !sourceStage.getSequenceNumber().equals(stageDto.getSequenceNumber())
+        ) {
+            if (seq < stageDto.getSequenceNumber()) {
+                throw new TypeServiceException("Sequence number is greater than the last sequence number");
+            }
+            Stage targetStage = stageRepository.findByTypeIdAndSequenceNumber(type.getId(), stageDto.getSequenceNumber())
+                    .orElseThrow(
+                            () -> new ObjectNotFoundException(
+                                    Stage.class.getSimpleName(),
+                                    stageDto.getSequenceNumber().toString()
+                            )
+                    );
+            targetStage.setSequenceNumber(sourceStage.getSequenceNumber());
+            stageRepository.save(targetStage);
+            sourceStage.setSequenceNumber(stageDto.getSequenceNumber());
+        }
+        return stageMapper.entityToDto(stageRepository.save(sourceStage));
     }
 }

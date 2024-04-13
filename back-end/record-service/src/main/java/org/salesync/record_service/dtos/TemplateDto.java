e5d939cf7fb21b6ba2d4026fb709e4b1fe8dad39
20120188 <20120188@student.hcmus.edu.vn>
2024-04-13 12:05:00 +0700

    add activity

--- back-end/record-service/src/main/java/org/salesync/record_service/dtos/TemplateDto.java
+++ back-end/record-service/src/main/java/org/salesync/record_service/dtos/TemplateDto.java
@@ -2,15 +2,18 @@ package org.salesync.record_service.dtos;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.util.UUID;
 
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class TypeDto {
+public class TemplateDto {
     private UUID id;
     private String name;
-    private TemplateDto template;
+
 }

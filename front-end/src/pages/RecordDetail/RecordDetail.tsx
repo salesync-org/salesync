99559e1ba94e7f7f8e45af04a46dd0ab97f4ec7e
mtoan12 <tranminhtoan1280@gmail.com>
2024-04-16 16:39:39 +0700

    Merge

--- front-end/src/pages/RecordDetail/RecordDetail.tsx
+++ front-end/src/pages/RecordDetail/RecordDetail.tsx
@@ -1,21 +1,21 @@
-import { Button, ButtonGroup, DropDown, Icon, Item, Panel } from '@/components/ui';
+import { Button, ButtonGroup, DropDownList, Icon, Item, Panel } from '@/components/ui';
 import { useState } from 'react';
 // import { useParams } from 'react-router-dom';
+import recordApi from '@/api/record';
+import NavigationButton from '@/components/NavigationButton/NavigationButton';
 import InputProperty from '@/components/RecordDetail/InputProperty';
+import RecordActivity from '@/components/RecordDetail/RecordActivity';
+import RecordTabs from '@/components/Records/RecordTabs';
 import RelationSections from '@/components/Relation/RelationSections';
 import StageSection from '@/components/Stage/StageSection';
 import LoadingSpinner from '@/components/ui/Loading/LoadingSpinner';
+import { MODAL_TYPES, useGlobalModalContext } from '@/context/GlobalModalContext';
 import useRecord from '@/hooks/record-service/useRecord';
 import useStages from '@/hooks/type-service/useStage';
+import useAuth from '@/hooks/useAuth';
 import { LayoutOrder, Stage } from '@/type';
 import { formatRecords } from '@/utils/utils';
 import { useParams } from 'react-router-dom';
-import recordApi from '@/api/record';
-import NavigationButton from '@/components/NavigationButton/NavigationButton';
-import useAuth from '@/hooks/useAuth';
-import RecordTabs from '@/components/Records/RecordTabs';
-import RecordActivity from '@/components/RecordDetail/RecordActivity';
-import { MODAL_TYPES, useGlobalModalContext } from '@/context/GlobalModalContext';
 
 const RecordDetail = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -33,10 +33,9 @@ const RecordDetail = () => {
 
   if (!record || !stages || !user) {
     return null;
-  }
-  else {
-    console.log('record', record.relations.length)
-    console.log(stages)
+  } else {
+    console.log('record', record.relations.length);
+    console.log(stages);
   }
 
   const mapStages = stages.map((stage: Stage) => {
@@ -133,15 +132,15 @@ const RecordDetail = () => {
           </ButtonGroup>
         </Panel>
 
-      {/* record contain  */}
-      <div className='grid grid-cols-4 md:grid-cols-4'>
-        <Panel className='col-span-1 mr-0 h-fit p-4'>
-          <GroupProperty name='About' data={dataAbout} className='mb-4' />
-          <GroupProperty name='Get in Touch' data={dataTouch} className='mb-4' />
-          <GroupProperty name='Segment' data={dataSegment} className='mb-4' />
-          <GroupProperty name='History' data={dataHistory} />
-        </Panel>
-
+        {/* record contain  */}
+        <div className='grid grid-cols-4 md:grid-cols-4'>
+          <Panel className='col-span-1 mr-0 h-fit p-4'>
+            {formattedRecord &&
+              Object.keys(formattedRecord).map((key) => {
+                if (key === 'id') return null;
+                return <InputProperty key={key} name={key} value={formattedRecord[key]} />;
+              })}
+          </Panel>
           <section className='col-span-2'>
             {stages && stages.length > 0 ? (
               <Panel className='order-3 col-span-2 h-fit p-4 md:order-none md:mr-0'>
@@ -155,26 +154,29 @@ const RecordDetail = () => {
               </Panel>
             )}
             <Panel className='order-3 col-span-2 h-fit p-4 md:order-none md:mr-0'>
-            <RecordActivity />
-          </Panel>
+              <RecordActivity />
+            </Panel>
           </section>
 
-        <section className='col-span-1'>
-          {record.relations.length !== 0 && <RelationSections relations={record.relations} />}
-          {record.relations.length === 0 && (
-            <Panel className='col-span-1 h-fit p-4'>
-            <div className='flex items-center'>
-              <div className='mr-2'>
-                <Icon name='merge' className='mr-1 rounded bg-orange-400 p-0.5 text-white'></Icon>
-              </div>
-              <div>
-                <span className='font-bold'>We found no potential duplicates of this {record.source_record.type.name}.</span>
-              </div>
-            </div>
-          </Panel>
-          )}
-        </section>
-      </div>
+          <section className='col-span-1'>
+            {record.relations.length !== 0 && <RelationSections relations={record.relations} />}
+            {record.relations.length === 0 && (
+              <Panel className='col-span-1 h-fit p-4'>
+                <div className='flex items-center'>
+                  <div className='mr-2'>
+                    <Icon name='merge' className='mr-1 rounded bg-orange-400 p-0.5 text-white'></Icon>
+                  </div>
+                  <div>
+                    <span className='font-bold'>
+                      We found no potential duplicates of this {record.source_record.type.name}.
+                    </span>
+                  </div>
+                </div>
+              </Panel>
+            )}
+          </section>
+        </div>
+      </section>
     </div>
   );
 };

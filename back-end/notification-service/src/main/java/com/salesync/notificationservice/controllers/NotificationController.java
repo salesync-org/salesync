7b97e08f5dc0b30e54953a3d4d0ca4ded150e1f3
20120188 <20120188@student.hcmus.edu.vn>
2024-04-17 16:07:14 +0700

    add NotificationController and NotificationService

--- back-end/notification-service/src/main/java/com/salesync/notificationservice/controllers/NotificationController.java
+++ back-end/notification-service/src/main/java/com/salesync/notificationservice/controllers/NotificationController.java
@@ -2,14 +2,21 @@ package com.salesync.notificationservice.controllers;
 
 
 import com.salesync.notificationservice.Services.INotificationService;
+import com.salesync.notificationservice.dtos.MessageDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.util.HtmlUtils;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @EnableWebSocketMessageBroker
@@ -32,5 +39,25 @@ public class NotificationController {
         return "ok";
     }
 
+    @GetMapping("/notifications/{receiverId}")
+    ResponseEntity<List<MessageDto>> getNotifications(@PathVariable UUID receiverId) {
+        return ResponseEntity.ok(notificationService.getNotificationsByReceiverId(receiverId));
+    }
+
+    @GetMapping("/unread-notifications/{receiverId}")
+    ResponseEntity<List<MessageDto>> getUnreadNotifications(@PathVariable UUID receiverId) {
+        return ResponseEntity.ok(notificationService.getUnreadNotificationsByReceiverId(receiverId));
+    }
+
+    @PutMapping("/set-notification-as-read/{messageId}")
+    ResponseEntity<MessageDto> setNotificationAsRead(@PathVariable UUID messageId) {
+        return ResponseEntity.ok(notificationService.setNotificationAsRead(messageId));
+    }
+
+    @PutMapping("/set-all-notification-as-read/{receiverId}")
+    ResponseEntity<List<MessageDto>> setAllNotificationsAsRead(@PathVariable UUID receiverId) {
+
+        return ResponseEntity.ok(notificationService.setAllNotificationsAsReadByReceiverId(receiverId));
+    }
 
 }

c438c1e9c12c02e5684b62f8e4431699b7bc0fdc
duytruong-23 <zduytruongz@gmail.com>
2024-04-21 21:20:56 +0700

    Write unit test for record service

--- back-end/record-service/src/main/java/org/salesync/record_service/dtos/CreateRecordRequestDto.java
+++ back-end/record-service/src/main/java/org/salesync/record_service/dtos/CreateRecordRequestDto.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import org.salesync.record_service.annotations.validation.contain_id.ContainId;
+import org.salesync.record_service.annotations.validation.is_required.IsRequired;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,8 +14,11 @@ import java.util.UUID;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Builder
 @Getter
+@Setter
 public class CreateRecordRequestDto {
+    @IsRequired
     private String recordName;
     private UUID stageId;
+    @ContainId
     List<RecordTypePropertyDto> properties;
 }

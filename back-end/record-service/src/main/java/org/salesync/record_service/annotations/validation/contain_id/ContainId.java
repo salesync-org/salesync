41938feacce8cebce997614865d5c00f3fc22dcc
duytruong-23 <zduytruongz@gmail.com>
2024-04-21 21:20:56 +0700

    Write unit test for record service

--- back-end/record-service/src/main/java/org/salesync/record_service/annotations/validation/contain_id/ContainId.java
+++ back-end/record-service/src/main/java/org/salesync/record_service/annotations/validation/contain_id/ContainId.java
@@ -1,16 +1,16 @@
-package org.salesync.record_service.annotations.validation;
+package org.salesync.record_service.annotations.validation.contain_id;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
-@Constraint(validatedBy = IsRequiredValidator.class)
-@Target({ElementType.FIELD, ElementType.TYPE})
+@Constraint(validatedBy = ContainIdValidator.class)
+@Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface IsRequired {
-    String message() default "is required";
+public @interface ContainId {
+    String message() default "must contain id field";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }

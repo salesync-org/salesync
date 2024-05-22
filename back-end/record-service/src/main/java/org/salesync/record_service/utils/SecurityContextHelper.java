6c582969751829f9f68009ebc3c78d2e14cc4f68
Võ Duy Trường <78472078+duytruong-23@users.noreply.github.com>
2024-05-22 21:42:41 +0700

    get permissions from token (#181)

--- back-end/record-service/src/main/java/org/salesync/record_service/utils/SecurityContextHelper.java
+++ back-end/record-service/src/main/java/org/salesync/record_service/utils/SecurityContextHelper.java
@@ -1,9 +1,17 @@
 package org.salesync.record_service.utils;
 
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityContextHelper {
     public static String getContextUserId() {
         return (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public static List<String> getContextAuthorities() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().map(
+                GrantedAuthority::getAuthority).toList();
     }
 }
